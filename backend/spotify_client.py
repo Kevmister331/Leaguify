b772756a815c094f81da9258381a436318515a11
stuartchen <stuartcc@student.ubc.ca>
2024-01-20 16:33:40 -0800

    changed the client credentials manager

--- backend/spotify_client.py
+++ backend/spotify_client.py
@@ -10,5 +10,4 @@ dotenv.load_dotenv()
 client_id = os.environ.get('CLIENT_ID')
 client_secret = os.environ.get('CLIENT_SECRET')
 def get_spotify_client():
-    client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
-    return spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    return spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
